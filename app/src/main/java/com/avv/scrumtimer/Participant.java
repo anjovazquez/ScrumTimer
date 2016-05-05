c76fc93b591ad9c766d21fe02daac49fd8f90c24
angelvazquez <anjo.vazquez@gmail.com>
2016-05-05 09:15:07 +0200

    Initial commit

---
+++ app/src/main/java/com/avv/scrumtimer/Participant.java
@@ -0,0 +1,24 @@
+package com.avv.scrumtimer;
+
+/**
+ * Created by angelvazquez on 4/5/16.
+ */
+public class Participant {
+
+    private String name;
+
+    public Participant() {
+    }
+
+    public Participant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
